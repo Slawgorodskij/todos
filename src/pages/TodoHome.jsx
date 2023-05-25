f54c99f27a5504e758affc9de1b45d24c5f0831b
vladimir <slawgorodskij@mail.ru>
2023-05-25 14:25:46 +0700

    исправлено игнорирование "вычеркивания"

--- src/pages/TodoHome.jsx
+++ src/pages/TodoHome.jsx
@@ -1,32 +1,33 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { deleteTodoAction, doneTodoAction } from "../store/todoAction";
-import { todoSelector } from "../store/todoSelector";
+import {useDispatch, useSelector} from "react-redux";
+import {Link} from "react-router-dom";
+import {deleteTodoAction, doneTodoAction} from "../store/todoAction";
+import {todoSelector} from "../store/todoSelector";
 
 export const TodoHome = () => {
     const dispatch = useDispatch();
     const todos = useSelector(todoSelector);
 
-    const setDone = (key) =>{
-        const newDeed = todos.find( (current) => current.key === key);     
+    const setDone = (key) => {
+        const newTodos = todos.slice()
+        const newDeed = newTodos.find((current) => current.key === key);
 
-        if(newDeed){       
-          newDeed.done=true
+        if (newDeed) {
+            newDeed.done = true
         }
 
-        dispatch(doneTodoAction(todos));
+        dispatch(doneTodoAction(newTodos));
     }
 
-    const deleteTodo = ( key ) => {
-       dispatch(deleteTodoAction(key));
+    const deleteTodo = (key) => {
+        dispatch(deleteTodoAction(key));
     }
 
     return (
         <section>
             <h1 className="title">Дела</h1>
-            {todos.map((item)=>(
-                <div 
-                    key={item.key} 
+            {todos.map((item) => (
+                <div
+                    key={item.key}
                     className="box columns is-mobile">
                     <div className="column is-for-fifths">
                         <Link to={`/${item.key}`}>
@@ -38,7 +39,7 @@ export const TodoHome = () => {
                         className="button is-success mr-5"
                         title="Пометить как сделаное"
                         disabled={item.done}
-                        onClick={() => setDone(item.key)}> 
+                        onClick={() => setDone(item.key)}>
                         &#9745;
                     </button>
                     <button
@@ -46,7 +47,7 @@ export const TodoHome = () => {
                         title="Удалить"
                         onClick={() => deleteTodo(item.key)}>
                         &#9746;
-                    </button> 
+                    </button>
                 </div>
             ))}
         </section>
